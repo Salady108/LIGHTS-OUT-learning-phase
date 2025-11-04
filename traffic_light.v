e164fd89ff40569fe584c56438be7180da342985
Salady108 <sriprahlad.241ec255@nitk.edu.in>
2025-11-04 17:55:00 +0530

    Refactor traffic light module to include color input and update state transitions

--- traffic_light.v
+++ traffic_light.v
@@ -1,5 +1,6 @@
-module traffic_light(input clk,input reset_n,output action);
+module traffic_light(input clk,input reset_n,input color,output action);
     reg [2:0] action;
+    wire [2:0] color;
     reg [2:0] state,next_State;
     parameter red=3'b000,
               green=3'b001,
@@ -12,9 +13,20 @@ module traffic_light(input clk,input reset_n,output action);
     end 
     always@(state)begin
         case(state)
-        red:next_State=yellow;
-        green:next_State=yellow;
-        yellow:next_State=green;
-        default:next_State=red;
+        red:next_state=color;
+        green:next_State=color;
+        yellow:next_State=color;
+        default:next_State=color;
         endcase 
     end
+    always@begin(state)begin
+        action=3'b011;
+        case(state)
+        red:action=3'b011;
+        yellow:action=3'b100;
+        green:action=3'b101;
+        default:action=3'b011;
+        endcase
+    end
+endmodule
+        
